acaf9f47fceb75cee2198c152c93ef1b79ed38bd
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-16 14:30:39 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-'use client'
+'use client';
 
-import { SignIn } from '@clerk/nextjs'
-
-export const dynamic = 'force-dynamic'
-
-export default function SignInPage() {
-  return <SignIn redirectUrl="/dashboard" />
+export default function Home() {
+  return (
+    <div className="min-h-screen p-4 bg-gray-50">
+      <div className="flex justify-end">
+      </div>
+    </div>
+  );
 }
+
