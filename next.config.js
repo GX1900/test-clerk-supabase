7bad5ff5c399291ba443dea15d107d13d52314e3
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-16 14:31:21 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: true, // ✅ Clerk v6で必要な設定
+  },
 };
 
 module.exports = nextConfig;
+
